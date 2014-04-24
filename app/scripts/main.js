88939772aa41fcd6ea843118f9d1d71a6da6d2c2
Daniel Santillan <daniel.santillan@eox.at>
2014-04-24 14:10:36 +0200

    Initial commit with scaffolding done with YO. Uses squery and bootstrap

---
+++ app/scripts/main.js
@@ -0,0 +1,22 @@
+
+
+$( document ).ready(function() {
+
+    $( ".col-md-6" ).mouseover(function(evt) {
+	  $(evt.currentTarget).css('background-color','#eee');
+	});
+
+	$( ".col-md-6" ).mouseout(function(evt) {
+	  $(evt.currentTarget).css('background-color','white');
+	});
+
+
+	$( ".col-md-6" ).click(function(evt) {
+	  $("#myModal").modal('show');
+	});
+
+
+
+});
+
+
